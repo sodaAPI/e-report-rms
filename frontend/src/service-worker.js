cdede20dedca456bc38a2daeb5431b71ef9b0665
sodapi <rafyrachmansyahh@gmail.com>
2023-02-13 10:41:06 +0700

    Fix fileExtensionRegexp

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -24,28 +24,24 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = /\/[^\/?][^\/]+\.[^\/]+$/;
+const fileExtensionRegexp = /\/[^\/?][^\/]+\.(html|css|js|json)$/;
 
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
-    // If this isn't a navigation, skip.
     if (request.mode !== "navigate") {
       return false;
-    } // If this is a URL that starts with /_, skip.
-
+    }
     if (url.pathname.startsWith("/_")) {
       return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
+    }
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    } // Return true to signal that we want to use the handler.
-
+    }
     return true;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
 );
+
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
